2b700f852fb30a5ca2559334f16b2f062474b4b1
박성재 <@210.178.175.58>
2021-07-08 14:08:16 +0900

    14.08

--- 20210708-라이브러리(컬렉션)/src/com/sist/dao/Movie.java
+++ 20210708-라이브러리(컬렉션)/src/com/sist/dao/Movie.java
@@ -9,4 +9,48 @@ public class Movie {
 	private String actor;
 	private String story;
 	private String key; // 동영상 (youtube)
+	// 읽기 / 쓰기 ==> 클래스로 사용자 정의 데이터형 ==> 반드시 (읽기/쓰기, 데이터 은닉화) => 캡슐화
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getActor() {
+		return actor;
+	}
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+	public String getStory() {
+		return story;
+	}
+	public void setStory(String story) {
+		this.story = story;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
 }
