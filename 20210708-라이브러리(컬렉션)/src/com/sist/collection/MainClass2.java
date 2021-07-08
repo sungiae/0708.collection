78bbadddb8ae5d1123b5f70cc7aaf8e0fac64a59
박성재 <@210.178.175.58>
2021-07-08 11:16:00 +0900

    11.15

---
+++ 20210708-라이브러리(컬렉션)/src/com/sist/collection/MainClass2.java
@@ -0,0 +1,11 @@
+package com.sist.collection;
+// java.util
+import java.util.*;
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 20210708-라이브러리(컬렉션)/src/com/sist/dao/Movie.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+
+// 1. 오라클 2. 파일 3. 웹에서 실시간 읽기
+public class Movie {
+	private int mno; // 영화 고유 번호
+	private String title;
+	private String genre;
+	private String director;
+	private String actor;
+	private String story;
+	private String key; // 동영상 (youtube)
+}
